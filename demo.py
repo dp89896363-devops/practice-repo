457b3187b775fa7f8b5c6d44b9b8fdaab60fc864
dp89896363-devops <dp89896363@gmail.com>
2026-02-22 14:53:58 +0530

    first commit

---
+++ demo.py
@@ -0,0 +1,11 @@
+# Program to add two numbers
+
+# Take input from the user
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+# Calculate sum
+sum = num1 + num2
+
+# Display result
+print("Sum =", sum)
